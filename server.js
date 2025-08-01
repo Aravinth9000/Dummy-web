7d48a084f267a5d1048c39cb5d05ff4cc795f26b
Aashman Kumar <aashman.b.kumar@gmail.com>
2025-08-01 14:44:34 +0100

    dumb mistake

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const users = ['bazingen']
 
-app.use(express.json)
+app.use(express.json())
 
 app.get('/users', (req, res) => {
     res.json(users)
